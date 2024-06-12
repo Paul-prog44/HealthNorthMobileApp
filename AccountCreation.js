f6989a444f9ebcac290260d7956b178239e86369
Paul-prog44 <saintouenfr@hotmail.com>
2024-06-12 16:12:36 +0200

    Account creation sending data to API ok

--- AccountCreation.js
+++ AccountCreation.js
@@ -1,10 +1,67 @@
-import { Text, View, Button, TextInput, ScrollView} from 'react-native'
+import { Button, ScrollView, Text} from 'react-native'
 import MyInput from './components/MyInput';
+import { useState } from 'react';
+import RNFetchBlob from 'rn-fetch-blob';
+
 
 export default function AccountCreation() {
+    const [newUser, setNewUser] = useState({
+        gender:"",
+        lastName:"",
+        firstName:"",
+        address:"",
+        city:"",
+        emailAddress:"",
+        password:"",
+        passwordConfirmation:"",
+        socialSecurity:""
+    })
+    const [errorCreatingUser, setErrorCreatingUser] = useState("")
+
+    function postUser() {
+        RNFetchBlob.config({ trusty: true })
+          .fetch(
+            'POST',
+            'https://192.168.1.109:8000/api/patients',
+            {'Content-Type' : 'application/json'},
+            JSON.stringify(newUser)
+          )
+          .then((resp) => {
+            // Vérifier si la réponse est correcte
+            if (resp.respInfo.status === 201) {
+              let userArray = JSON.parse(resp.data)
+              setUser(userArray)
+            } else {
+            //   setInvalidCredentials(true)
+            }
+          })
+          .catch((error) => {
+            console.error('Error downloading file: ', error);
+          });
+      }
+
+    handleSubmit = (() => {
+        postUser()
+    })
+
+    
+
     return (
         <ScrollView style={{flex:1, flexDirection:"column", backgroundColor:"rgb(169, 221, 242)"}}>
-            <MyInput label="Nom" placeholder="Durant"/>
+                    <Text>{newUser.gender}</Text>
+
+            <MyInput label="Sexe" placeholder="M" inputMode="text" setNewUser={setNewUser} name="gender"/>
+            <MyInput label="Nom" placeholder="Durant" inputMode="text" setNewUser={setNewUser} name="lastName"/>
+            <MyInput label="Prénom" placeholder="Benard" inputMode="text" setNewUser={setNewUser} name="firstName"/>
+            <MyInput label="Adresse" placeholder="5 rue des corneilles" inputMode="text" setNewUser={setNewUser} name="address"/>
+            <MyInput label="Ville" placeholder="Marseille" inputMode="text" setNewUser={setNewUser} name="city"/>
+            <MyInput label="Numéro de sécurité sociale" placeholder="0123456789" inputMode="decimal" setNewUser={setNewUser} name="socialSecurity"/>
+            <MyInput label="Adresse email" placeholder="bdurant@caramail.com" inputMode="email" setNewUser={setNewUser} name="emailAddress"/>
+            <MyInput label="Mot de passe" placeholder="" secure={true} setNewUser={setNewUser} name="password"/>
+            <MyInput label="Vérification mot de passe" placeholder="" secure={true} setNewUser={setNewUser} name="passwordConfirmation"/>
+            {errorCreatingUser && "Une erreur est survenu, merci de réessayer"}
+            <Button title="Créer mon compte" onPress={handleSubmit}/>
+
         </ScrollView>
       );
 }
--- components/MyInput.jsx
+++ components/MyInput.jsx
@@ -1,15 +1,36 @@
 
 import { Text, TextInput, View } from "react-native";
 
-export default function mMInput({label, placeholder}) {
+export default function mMInput({label, placeholder, inputMode, secure, setNewUser , name}) {
+    const handleChange = ((key, value) => {
+        setNewUser(prevState => ({
+            ...prevState,
+            [key]:value
+        }))
+    })
 
     return (
-        <View>
-            <Text style={{fontWeight:'bold', fontSize:20}}>{label} :</Text>
+        <View style={{
+            alignItems:"center"
+        }}>
+            <Text style={{
+                fontWeight:'bold', 
+                fontSize:20
+                }}>{label} :
+            </Text>
             <TextInput
-                style={{height: 40, width: 300, backgroundColor : "white" , marginBottom: 20, borderColor: 'gray',
-                    borderWidth: 1,}}
+                style={{
+                    height: 40, 
+                        width: 300, 
+                        backgroundColor : "white" , 
+                        marginBottom: 20, 
+                        borderColor: 'gray',
+                        borderWidth: 1
+                    }}
                 placeholder={placeholder}
+                inputMode={inputMode}
+                secureTextEntry={secure}
+                onChangeText={(text) => handleChange(name, text)}
             />
         </View>
     )
